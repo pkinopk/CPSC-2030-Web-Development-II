d0fd54782490a1615d8fd0e4b68ea68c882b66c4
HomeDesktop <pkinopk@gmail.com>
2018-02-04 11:29:54 -0800

    Done

--- Assignments/Assignment-5/a4_starter_files/aria-menu.js
+++ Assignments/Assignment-5/a4_starter_files/aria-menu.js
@@ -1,21 +1,21 @@
 $(function () {
 
 	$("a[id*='menu-button']").on("click", function (e) {
-		// e.preventDefault();
+		e.preventDefault();
 		$(this).parent().find("ul").toggle();
 	});
 
-	$("#menu-button1").on("keydown", function (e) {
+	$("a[id*='menu-button']").on("keydown", function (e) {
 		e.preventDefault();
 		if (e.which == 32 || e.which == 40 || e.which == 13) {
-			$("#main-menu").toggle();
+			$(this).parent().find("ul").toggle();
 			$(this).parent().find("li:first-child > a").focus();
 
 		}
 	});
 
 	var posititon = 1;
-	$("#main-menu").on("keydown", function (e) {
+	$("[id*='-menu']").on("keydown", function (e) {
 
 		if (posititon == 5 && e.which == 40) { // LAST MENU ITEM
 			posititon = 0;
@@ -34,8 +34,7 @@ $(function () {
 			$(this).find("li:nth-child(" + posititon + ") > a").focus();
 		}
 		if (e.which == 27) { // ESC
-			$("#main-menu").toggle();
-			// $("#menu-button1").focus();
+			$(this).toggle();
 			posititon = 1;
 		}
 
